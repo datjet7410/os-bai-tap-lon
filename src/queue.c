8a6f42e4a5e532557a1f7c618c064e6c083ecce3
Nguyen Tien Dat <dat.nguyendatjet7410@hcmut.edu.vn>
2022-11-23 12:15:24 +0700

    delete unused code

--- src/queue.c
+++ src/queue.c
@@ -18,21 +18,15 @@ struct pcb_t * dequeue(struct queue_t * q) {
 	/* TODO: return a pcb whose prioprity is the highest
 	 * in the queue [q] and remember to remove it from q
 	 * */
-	if (empty(q)) return NULL;  // if queue size = 0, do nothing
+	if (empty(q)) return NULL; // if queue size = 0, do nothing
 
-	int highest_prio_idx = 0;
+	struct pcb_t * res = q->proc[0]; // get it out
 
-	for (int i = 1; i < q->size; i++) { // traverse to find highest prior. index
- 		if (q->proc[i]->prio < q->proc[highest_prio_idx]->prio) {
-			highest_prio_idx = i;
-		}
+	// shift back to rearrange the queue
+	for (int i = 1; i < q->size; i++) {
+		q->proc[i-1] = q->proc[i];
 	}
 
-	struct pcb_t * res = q->proc[highest_prio_idx]; // get it out
-	for (int i = highest_prio_idx + 1; i < q->size; i++) {
-		q->proc[i-1] = q->proc[i];  // shift back to rearrange the queue
-	}
-	
 	q->size--;
 
 	return res;
